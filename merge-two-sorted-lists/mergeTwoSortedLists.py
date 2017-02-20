fc4354da9817012b842bf2affdb9e38c0cf17f35
nan <nan@sensorhound.com>
2017-02-20 09:55:20 -0500

    merge two sorted lists

---
+++ merge-two-sorted-lists/mergeTwoSortedLists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        dummy = head
+        while l1 != None or l2 != None:
+            if l1 == None:
+                dummy.next = l2
+                break
+            elif l2 == None:
+                dummy.next = l1
+                break
+            if l1.val < l2.val:
+                dummy.next = l1
+                l1 = l1.next
+            else:
+                dummy.next = l2
+                l2 = l2.next
+            dummy = dummy.next
+        return head.next
