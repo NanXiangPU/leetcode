d7332a005a5185a2b1730e4e6e510cb2ca9d8f29
nan <nan@sensorhound.com>
2017-03-24 15:57:18 -0400

    intersection of two sorted array II

---
+++ intersection-of-two-arrays-II/intersectionOfTwoArraysII.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if nums1 == None or len(nums1) == 0 or nums2 == None or len(nums2) == 0:
+            return []
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        i = j = 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return res
