4a15a55570d2d16feb115da22632a19071a4fc01
nan <nan@sensorhound.com>
2017-07-08 17:28:15 -0400

    new

---
+++ longest-substring-without-repeating-character/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                start = Math.max(map.get(c) + 1, start);
+            }
+            map.put(c, i);
+            maxLen = Math.max(maxLen, i - start + 1);
+        }
+        return maxLen;
+    }
+}
